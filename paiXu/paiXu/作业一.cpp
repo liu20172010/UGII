6f171b58df06e1cfac4c2554c2197708d3330cc0
Mr.Liu <liu20172010@gmail.com>
2018-03-05 14:00:16 +0800

    给定数组排序

---
+++ paiXu/paiXu/作业一.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+int main()
+{
+	int a[] = { 21, 52, 13, 69, 58, 43, 22, 15, 8 };
+	for (int i = 0; i < 9; i++)
+	{
+		for (int j = i+1; j < 9; j++)
+		{
+
+			if (a[i]>a[j])
+			{
+				a[i] = a[i] ^ a[j];
+				a[j] = a[i] ^ a[j];
+				a[i] = a[i] ^ a[j];
+			}		
+		}
+
+	}
+	for (int i = 0; i < 9; i++)
+	{
+		cout << a[i] << "  " ;
+	}
+	system("pause");
+	return 0;
+}
