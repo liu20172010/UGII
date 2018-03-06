81aadf8e47a124fbe8719c7d7ef6a72503073f28
Mr.Liu <liu20172010@gmail.com>
2018-03-06 22:09:19 +0800

    冒泡法定稿

--- paiXu/paiXu/作业一.cpp
+++ paiXu/paiXu/作业一.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main()
 {
-	int a[] = { 21, 52, 13, 69, 58, 43, 22, 15, 8 };
+	int a[] = { 21, 52, 13, 69, 58, 43, 22, 15, 8 ,2,2,2,2,1};
 	cout << "ÅÅĞòÇ°£º";
 	for (int i = 0; i < 9; i++)
 	{
@@ -14,9 +14,10 @@ int main()
 	}
 	cout << endl;
 
+
 	for (int i = 0; i < 9; i++)
 	{
-		for (int j = i+1; j < 9; j++)
+		for (int j = i + 1; j < 9; j++)
 		{
 
 			if (a[i]>a[j])
